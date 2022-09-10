d329f90810daa77517e45e95a0c4e0bdaf912e3a
Bino <yeebino001@gmail.com>
2022-09-10 15:44:03 +0200

    study & comments

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|route mi connette la rotta ad una view 
+    Route::get('/', function () {
+        return view('welcome');
+    });
+|tuttavia voglio che a gestire ciò sia il controller (il nome del controller deve essere singolare)
+    Route::get('/', "NomeDelController@MetodoController");
+|"a questo punto abbiamo collegato le due cose e ciò che stiamo dicendo è vai su NomeDelController e recupera questa informazione (MetodoController)"
+|
+|
+*/
+ 
+Route::get('/', "HomeController@GiovanniMucciaccia");
